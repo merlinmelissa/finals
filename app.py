0a967632bb23a005d681f5f0f4b069efe58e4169
merlinmelissa <merlinmelissa@gmail.com>
2026-09-09 15:36:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ SAMPLES = {
         "ground_truth": "Benign"
     },
     "Sample 18": {
-        "path": "samples/benign_13.jpg",
+        "path": "samples/benign_14.jpg",
         "ground_truth": "Benign"
     },
     "Sample 19": {
